74c3cce49e3b7ce45f1eaf273df319a3a634416d
Your Name <rubygems@chrislee.dhs.org>
2025-05-31 23:07:09 -0400

    bug fixes for e5 and openai

--- orangecontrib/nlp/widgets/owmodel_e5.py
+++ orangecontrib/nlp/widgets/owmodel_e5.py
@@ -1,6 +1,7 @@
 from orangecontrib.nlp.util.embedder_models import EmbedderModel
 from Orange.widgets.widget import Output, OWWidget
 from transformers import AutoTokenizer, AutoModel
+import numpy as np
 
 # TODO: Make this thread safe
 class E5Embedder(EmbedderModel):
--- orangecontrib/nlp/widgets/owmodel_openai.py
+++ orangecontrib/nlp/widgets/owmodel_openai.py
@@ -2,23 +2,25 @@ from orangecontrib.nlp.util.embedder_models import EmbedderModel
 from Orange.widgets.widget import Output, OWWidget
 from Orange.widgets.settings import Setting
 from AnyQt.QtWidgets import (
-    QLineEdit, QLabel
+    QLineEdit, QLabel, QComboBox
 )
 from AnyQt.QtCore import Qt
 import numpy as np
 import openai
+from typing import get_args
 
 class OpenAIEmbedder(EmbedderModel):
-    def __init__(self, api_key):
+    def __init__(self, api_key, model="text-embedding-3-small"):
         self.api_key = api_key
+        self.model = model
 
     def embed(self, language, texts):
-        openai.api_key = self.api_key
-        response = openai.Embedding.create(
+        client = openai.OpenAI(api_key=self.api_key)
+        response = client.embeddings.create(
             input=texts,
-            model="text-embedding-ada-002"
+            model=self.model
         )
-        return np.array([item["embedding"] for item in response["data"]], dtype="float32")
+        return np.array([item.embedding for item in response.data], dtype="float32")
 
 class OWOpenAIEmbedder(OWWidget):
     name = "OpenAI Embedder"
@@ -33,22 +35,44 @@ class OWOpenAIEmbedder(OWWidget):
     want_control_area = True
 
     api_key = Setting("")
+    openai_model = Setting("text-embedding-3-small")
  
     def __init__(self):
         super().__init__()
-        self.Outputs.embedder.send(OpenAIEmbedder(self.api_key))
         self.layout_control_area()
+        self.update()
 
     def layout_control_area(self):
         self.controlArea.layout().addWidget(QLabel("API Key:"))
         self.api_key_input = QLineEdit(self.api_key)
         self.api_key_input.editingFinished.connect(self.on_api_key_changed)
         self.controlArea.layout().addWidget(self.api_key_input)
-        self.controlArea.layout().setAlignment(Qt.AlignTop)     
+
+        embedding_models = get_args(openai.types.EmbeddingModel)
+        self.model_combo = QComboBox()
+        self.model_combo.addItems(embedding_models)
+        self.model_combo.setCurrentText(self.openai_model)
+        self.model_combo.currentTextChanged.connect(self.on_change_model)
+
+        self.controlArea.layout().addWidget(QLabel("Embedding Model:"))
+        self.controlArea.layout().addWidget(self.model_combo)
+        
+        self.controlArea.layout().setAlignment(Qt.AlignTop)
 
     def on_api_key_changed(self):
         self.api_key = self.api_key_input.text()
-        self.Outputs.embedder.send(OpenAIEmbedder(self.api_key))
+        self.update()
+
+    def on_change_model(self, val):
+        self.openai_model = val
+        self.update()
+
+    def update(self):
+        if self.api_key and self.openai_model:
+            self.Outputs.embedder.send(OpenAIEmbedder(self.api_key, self.openai_model))
+        else:
+            self.Outputs.embedder.send(None)
+            print("None")
 
 if __name__ == "__main__":
     from Orange.widgets.utils.widgetpreview import WidgetPreview
